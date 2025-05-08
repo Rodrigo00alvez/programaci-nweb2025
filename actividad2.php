4fd9e7e4b4966cd313d0afd34c9d614692b8cd20
rodrigo00alvez <rodrigo27021@gmail.com>
2025-05-08 15:35:41 -0300

    actividad de conversion de pesos

---
+++ actividad2.php
@@ -0,0 +1,27 @@
+<?php
+if($_SERVER ["REQUEST_METHOD"] == "POST"){
+$cantidadEnPesos = floatval($_POST["pesos"]);
+$valorDolar = 40;
+$valorEuro = 43;
+$valorReal = 8;
+switch($moneda){
+    case "dolares":
+        $cantidadTotalDolares = $cantidadEnPesos / $valorDolar;
+        echo "tenes $cantidadTotalDolares";
+        break;
+    case "euros":
+        $cantidadTotalEuros = $cantidadEnPesos / $valorEuro;
+        echo "tenes $cantidadTotalEuros";
+        break;
+    case "real":
+        $cantidadTotalReal = $cantidadEnPesos / $valorReal;
+        echo "tenes $cantidadTotalReal";
+        break;
+
+}
+
+}else{
+    echo"error en la conexion";
+}
+
+?>
